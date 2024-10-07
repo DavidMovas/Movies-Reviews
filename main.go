7f53642036751c859fbe13857eae4be12aa747d8
Nofre <davidprogram417@gmail.com>
2024-10-07 22:04:44 +0200

    small main update

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/DavidMovas/Movies-Reviews/internal/config"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/labstack/echo"
+)
+
+var (
+	dbConnectionTime = time.Second * 10
 )
 
 func main() {
@@ -17,13 +23,19 @@ func main() {
 	cfg, err := config.NewConfig()
 	failOnError(err, "failed to load config")
 
+	db, err := getDB(context.Background(), cfg.DBUrl)
+	failOnError(err, "failed to connect to db")
+
+	if err := db.Ping(context.Background()); err != nil {
+		failOnError(err, "failed to ping db")
+	}
+
 	//TODO: Add signal's listener in goroutine to shutdown gracefully
 	go func() {
 		signalCh := make(chan os.Signal)
 		signal.Notify(signalCh, os.Interrupt, os.Kill)
 
 		ctx, cancel := context.WithTimeout(context.Background(), 10)
-		ctx, cancel = signal.NotifyContext(ctx, os.Interrupt, os.Kill)
 		defer cancel()
 
 		for {
@@ -42,6 +54,17 @@ func main() {
 	}
 
 	log.Printf("Server closed")
+}
+
+func getDB(ctx context.Context, connString string) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(ctx, dbConnectionTime)
+	defer cancel()
+
+	db, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to db: %w", err)
+	}
+	return db, err
 }
 
 func failOnError(err error, msg string) {
