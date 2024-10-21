361bc821903eff87ad56c86a67d400a0082b96c8
Nofre <davidprogram417@gmail.com>
2024-10-21 18:29:44 +0200

    added new movies contract

---
+++ contracts/movies.go
@@ -0,0 +1,16 @@
+package contracts
+
+import "time"
+
+type Movie struct {
+	ID          int        `json:"id"`
+	Title       string     `json:"title"`
+	ReleaseDate time.Time  `json:"releaseDate"`
+	CreatedAt   time.Time  `json:"createdAt"`
+	DeletedAt   *time.Time `json:"deletedAt,omitempty"`
+}
+
+type MovieDetails struct {
+	Movie
+	Description string `json:"description"`
+}
