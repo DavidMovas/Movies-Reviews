8f484576ce5db5ee60aead3309fa48664698b320
Nofre <davidprogram417@gmail.com>
2024-10-30 21:58:44 +0100

    review's repository implemented

--- internal/modules/reviews/models.go
+++ internal/modules/reviews/models.go
@@ -1,17 +1,21 @@
 package reviews
 
-import "github.com/DavidMovas/Movies-Reviews/internal/pagination"
+import (
+	"time"
+
+	"github.com/DavidMovas/Movies-Reviews/internal/pagination"
+)
 
 type Review struct {
-	ID          int     `json:"id"`
-	MovieID     int     `json:"movieId"`
-	UserID      string  `json:"userId"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Rating      int     `json:"rating"`
-	CreatedAt   string  `json:"createdAt"`
-	UpdatedAt   *string `json:"updatedAt,omitempty"`
-	DeletedAt   *string `json:"deletedAt,omitempty"`
+	ID        int        `json:"id"`
+	MovieID   int        `json:"movieId"`
+	UserID    string     `json:"userId"`
+	Rating    int        `json:"rating"`
+	Title     string     `json:"title"`
+	Content   string     `json:"description"`
+	CreatedAt time.Time  `json:"createdAt"`
+	UpdatedAt *time.Time `json:"updatedAt,omitempty"`
+	DeletedAt *time.Time `json:"deletedAt,omitempty"`
 }
 
 type GetReviewRequest struct {
@@ -29,19 +33,19 @@ type GetReviewsByUserIDRequest struct {
 }
 
 type CreateReviewRequest struct {
-	MovieID     int    `json:"-" param:"movieId" validate:"nonzero"`
-	UserID      int    `json:"userId" validate:"nonzero"`
-	Title       string `json:"title" validate:"max=100"`
-	Description string `json:"description" validate:"max=1000"`
-	Rating      int    `json:"rating" validate:"min=1,max=10"`
+	MovieID int    `json:"-" param:"movieId" validate:"nonzero"`
+	UserID  int    `json:"userId" validate:"nonzero"`
+	Title   string `json:"title" validate:"max=100"`
+	Content string `json:"description" validate:"max=1000"`
+	Rating  int    `json:"rating" validate:"min=1,max=10"`
 }
 
 type UpdateReviewRequest struct {
-	ReviewID    int     `json:"-" param:"reviewId" validate:"nonzero"`
-	MovieID     int     `json:"movieId" validate:"nonzero"`
-	Title       *string `json:"title,omitempty" validate:"max=100"`
-	Description *string `json:"description,omitempty" validate:"max=1000"`
-	Rating      *int    `json:"rating,omitempty" validate:"min=1,max=10"`
+	ReviewID int     `json:"-" param:"reviewId" validate:"nonzero"`
+	MovieID  int     `json:"movieId" validate:"nonzero"`
+	Title    *string `json:"title,omitempty" validate:"max=100"`
+	Content  *string `json:"description,omitempty" validate:"max=1000"`
+	Rating   *int    `json:"rating,omitempty" validate:"min=1,max=10"`
 }
 
 type DeleteReviewRequest struct {
--- internal/modules/reviews/repository.go
+++ internal/modules/reviews/repository.go
@@ -1,6 +1,19 @@
 package reviews
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	apperrors "github.com/DavidMovas/Movies-Reviews/internal/error"
+	"github.com/jackc/pgx/v5"
+
+	"github.com/Masterminds/squirrel"
+
+	"github.com/DavidMovas/Movies-Reviews/internal/dbx"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type Repository struct {
 	db *pgxpool.Pool
@@ -10,4 +23,209 @@ func NewRepository(db *pgxpool.Pool) *Repository {
 	return &Repository{
 		db: db,
 	}
+}
+
+func (r *Repository) GetReviewsByMovieID(ctx context.Context, movieID int, offset int, limit int, sort, order string) ([]*Review, int, error) {
+	selectQuery := dbx.StatementBuilder.Select("id, movie_id, user_id, rating, title, content, created_at, updated_at, deleted_at").
+		From("reviews").
+		Where("movie_id = ?", movieID).
+		Where(squirrel.Eq{"deleted_at": nil}).
+		Offset(uint64(offset)).
+		Limit(uint64(limit)).
+		OrderBy(sort + " " + order)
+
+	countQuery := dbx.StatementBuilder.Select("COUNT(*)").
+		From("reviews").
+		Where("movie_id = ?", movieID).
+		Where(squirrel.Eq{"deleted_at": nil})
+
+	b := &pgx.Batch{}
+	if err := dbx.QueryBatchSelect(b, selectQuery); err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+	if err := dbx.QueryBatchSelect(b, countQuery); err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+
+	br := r.db.SendBatch(ctx, b)
+	defer func() {
+		_ = br.Close()
+	}()
+
+	rows, err := br.Query()
+	if err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+	defer rows.Close()
+
+	reviews, err := pgx.CollectRows[*Review](rows, pgx.RowToAddrOfStructByPos[Review])
+	if err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+
+	var total int
+	if err = br.QueryRow().Scan(&total); err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+
+	return reviews, total, nil
+}
+
+func (r *Repository) GetReviewsByUserID(ctx context.Context, userID int, offset int, limit int, sort, order string) ([]*Review, int, error) {
+	selectQuery := dbx.StatementBuilder.Select("id, movie_id, user_id, rating, title, content, created_at, updated_at, deleted_at").
+		From("reviews").
+		Where("user_id = ?", userID).
+		Where(squirrel.Eq{"deleted_at": nil}).
+		Offset(uint64(offset)).
+		Limit(uint64(limit)).
+		OrderBy(sort + " " + order)
+
+	countQuery := dbx.StatementBuilder.Select("COUNT(*)").
+		From("reviews").
+		Where("user_id = ?", userID).
+		Where(squirrel.Eq{"deleted_at": nil})
+
+	b := &pgx.Batch{}
+	if err := dbx.QueryBatchSelect(b, selectQuery); err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+	if err := dbx.QueryBatchSelect(b, countQuery); err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+
+	br := r.db.SendBatch(ctx, b)
+	defer func() {
+		_ = br.Close()
+	}()
+
+	rows, err := br.Query()
+	if err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+	defer rows.Close()
+
+	reviews, err := pgx.CollectRows[*Review](rows, pgx.RowToAddrOfStructByPos[Review])
+	if err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+
+	var total int
+	if err = br.QueryRow().Scan(&total); err != nil {
+		return nil, 0, apperrors.Internal(err)
+	}
+
+	return reviews, total, nil
+}
+
+func (r *Repository) GetReviewByID(ctx context.Context, reviewID int) (*Review, error) {
+	builder := dbx.StatementBuilder.Select("id, movie_id, user_id, rating, title, content, created_at, updated_at, deleted_at").
+		From("reviews").
+		Where("id = ?", reviewID).
+		Where(squirrel.Eq{"deleted_at": nil})
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, apperrors.Internal(err)
+	}
+
+	rows, err := r.db.Query(ctx, query, args...)
+	if err != nil {
+		return nil, apperrors.Internal(err)
+	}
+	defer rows.Close()
+
+	return pgx.RowToAddrOfStructByPos[Review](rows)
+}
+
+func (r *Repository) CreateReview(ctx context.Context, req *CreateReviewRequest) error {
+	builder := dbx.StatementBuilder.Insert("reviews").
+		Columns("movie_id", "user_id", "rating", "title", "content").
+		Values(req.MovieID, req.UserID, req.Rating, req.Title, req.Content)
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return apperrors.Internal(err)
+	}
+
+	n, err := r.db.Exec(ctx, query, args...)
+	if err != nil {
+		return apperrors.Internal(err)
+	}
+
+	if n.RowsAffected() == 0 {
+		return apperrors.AlreadyExists("review", "user_id", req.UserID)
+	}
+
+	return nil
+}
+
+func (r *Repository) UpdateReview(ctx context.Context, reviewID int, req *UpdateReviewRequest) (*Review, error) {
+	if req.Rating == nil && req.Title == nil && req.Content == nil {
+		return nil, apperrors.BadRequest(fmt.Errorf("no fields to update"))
+	}
+
+	builder := dbx.StatementBuilder.Update("reviews").
+		Set("updated_at", time.Now()).
+		Where("id = ?", reviewID).
+		Where(squirrel.Eq{"deleted_at": nil}).
+		Suffix("RETURNING id, movie_id, user_id, rating, title, content, created_at, updated_at, deleted_at")
+
+	if req.Rating != nil {
+		builder = builder.Set("rating", *req.Rating)
+	}
+	if req.Title != nil {
+		builder = builder.Set("title", *req.Title)
+	}
+	if req.Content != nil {
+		builder = builder.Set("content", *req.Content)
+	}
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, apperrors.Internal(err)
+	}
+
+	var review Review
+	err = r.db.QueryRow(ctx, query, args...).
+		Scan(&review.ID, &review.MovieID, &review.UserID, &review.Rating, &review.Title, &review.Content, &review.CreatedAt, &review.UpdatedAt, &review.DeletedAt)
+
+	switch {
+	case dbx.IsNoRows(err):
+		return nil, apperrors.NotFound("review", "id", reviewID)
+	case err != nil:
+		return nil, apperrors.Internal(err)
+	}
+
+	return &review, nil
+}
+
+func (r *Repository) DeleteReview(ctx context.Context, reviewID int) error {
+	builder := dbx.StatementBuilder.Update("reviews").
+		Set("deleted_at", time.Now()).
+		Where("id = ?", reviewID).
+		Where(squirrel.Eq{"deleted_at": nil})
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return apperrors.Internal(err)
+	}
+
+	n, err := r.db.Exec(ctx, query, args...)
+	if err != nil {
+		return apperrors.Internal(err)
+	}
+
+	if n.RowsAffected() == 0 {
+		return apperrors.NotFound("review", "id", reviewID)
+	}
+
+	return nil
 }
