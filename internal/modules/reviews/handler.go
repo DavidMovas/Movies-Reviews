89561fdc964b8d4015aed2688e2920f1e6c0afd1
Nofre <davidprogram417@gmail.com>
2024-10-30 21:07:51 +0100

    added models for reviews

--- internal/modules/reviews/handler.go
+++ internal/modules/reviews/handler.go
@@ -1,6 +1,11 @@
 package reviews
 
-import "github.com/DavidMovas/Movies-Reviews/internal/config"
+import (
+	"net/http"
+
+	"github.com/DavidMovas/Movies-Reviews/internal/config"
+	"github.com/labstack/echo/v4"
+)
 
 type Handler struct {
 	service          *Service
@@ -12,4 +17,28 @@ func NewHandler(service *Service, paginationConfig *config.PaginationConfig) *Ha
 		service:          service,
 		paginationConfig: paginationConfig,
 	}
+}
+
+func (h *Handler) GetReviewsByMovieID(c echo.Context) error {
+	return c.String(http.StatusOK, "not implemented")
+}
+
+func (h *Handler) GetReviewsByUserID(c echo.Context) error {
+	return c.String(http.StatusOK, "not implemented")
+}
+
+func (h *Handler) GetReviewByID(c echo.Context) error {
+	return c.String(http.StatusOK, "not implemented")
+}
+
+func (h *Handler) CreateReview(c echo.Context) error {
+	return c.String(http.StatusOK, "not implemented")
+}
+
+func (h *Handler) UpdateReview(c echo.Context) error {
+	return c.String(http.StatusOK, "not implemented")
+}
+
+func (h *Handler) DeleteReview(c echo.Context) error {
+	return c.String(http.StatusOK, "not implemented")
 }
--- internal/modules/reviews/models.go
+++ internal/modules/reviews/models.go
@@ -1,1 +1,49 @@
 package reviews
+
+import "github.com/DavidMovas/Movies-Reviews/internal/pagination"
+
+type Review struct {
+	ID          int     `json:"id"`
+	MovieID     int     `json:"movieId"`
+	UserID      string  `json:"userId"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Rating      int     `json:"rating"`
+	CreatedAt   string  `json:"createdAt"`
+	UpdatedAt   *string `json:"updatedAt,omitempty"`
+	DeletedAt   *string `json:"deletedAt,omitempty"`
+}
+
+type GetReviewRequest struct {
+	ReviewID int `json:"-" param:"reviewId" validate:"nonzero"`
+}
+
+type GetReviewsByMovieIDRequest struct {
+	pagination.PaginatedRequestOrdered
+	MovieID int `json:"-" param:"movieId" validate:"nonzero"`
+}
+
+type GetReviewsByUserIDRequest struct {
+	pagination.PaginatedRequestOrdered
+	UserID int `json:"-" param:"userId" validate:"nonzero"`
+}
+
+type CreateReviewRequest struct {
+	MovieID     int    `json:"-" param:"movieId" validate:"nonzero"`
+	UserID      int    `json:"userId" validate:"nonzero"`
+	Title       string `json:"title" validate:"max=100"`
+	Description string `json:"description" validate:"max=1000"`
+	Rating      int    `json:"rating" validate:"min=1,max=10"`
+}
+
+type UpdateReviewRequest struct {
+	ReviewID    int     `json:"-" param:"reviewId" validate:"nonzero"`
+	MovieID     int     `json:"movieId" validate:"nonzero"`
+	Title       *string `json:"title,omitempty" validate:"max=100"`
+	Description *string `json:"description,omitempty" validate:"max=1000"`
+	Rating      *int    `json:"rating,omitempty" validate:"min=1,max=10"`
+}
+
+type DeleteReviewRequest struct {
+	ReviewID int `json:"-" param:"reviewId" validate:"nonzero"`
+}
