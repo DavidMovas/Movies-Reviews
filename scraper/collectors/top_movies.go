e6819b41c77899ca5636d4e3c56bdad24012779f
Nofre <davidprogram417@gmail.com>
2024-11-05 21:48:13 +0100

    created top_movie scraper collector

---
+++ scraper/collectors/top_movies.go
@@ -0,0 +1,42 @@
+package collectors
+
+import (
+	"log/slog"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+type TopMoviesCollector struct {
+	c *colly.Collector
+	l *slog.Logger
+}
+
+func NewTopMoviesCollector(c *colly.Collector, movieCollector *MovieCollector, logger *slog.Logger) *TopMoviesCollector {
+	collector := &TopMoviesCollector{
+		c: c,
+		l: logger.With("collector", "top_movies"),
+	}
+
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		e.ForEach("a[href]", func(i int, e *colly.HTMLElement) {
+			link := e.Attr("href")
+			text := strings.TrimSpace(e.Text)
+
+			if text != "" && strings.HasPrefix(link, "/title/") {
+				movieCollector.Visit(e.Request.AbsoluteURL(link))
+			}
+		})
+	})
+
+	return collector
+}
+
+func (c *TopMoviesCollector) Star() {
+	starLink := "https://www.imdb.com/chart/top"
+	visit(c.c, starLink, c.l)
+}
+
+func (c *TopMoviesCollector) Wait() {
+	c.c.Wait()
+}
